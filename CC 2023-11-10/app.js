6573bcaac7877ea02dc1e32472b106334ff90fe3
mclark <maryariley4578@yahoo.com>
2023-11-10 19:08:12 -0500

    add coding challenges changes for 11-10-23

---
+++ CC 2023-11-10/app.js
@@ -0,0 +1,10 @@
+// 11.10.23
+
+// function removeExclamationMarks(s) {
+// //  return s.replace('!', '');
+// }
+//mine should have been replace all
+
+function removeExclamationMarks(s) {
+  return s.replaceAll("!", "");
+}
