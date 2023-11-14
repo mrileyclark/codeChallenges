0dea95906502d24859825a93d539b773401d54b2
mclark <maryariley4578@yahoo.com>
2023-11-13 19:06:27 -0500

    add coding challenges changes for 11-13-23

---
+++ CC 2023-11-13/app.js
@@ -0,0 +1,13 @@
+// 11.13.23
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+function numberToString(num) {
+  return num.toString();
+}
+
+// const numberToString = num => `${num}`;
+
+// const numberToString = num => num.toString();
