092b75d01a37aefd7855c7adda3d2591778ed04c
mclark <maryariley4578@yahoo.com>
2023-11-14 19:05:57 -0500

    add coding challenges changes for 11-14-23

---
+++ CC 2023-11-14/app.js
@@ -0,0 +1,19 @@
+// 11.14.23
+
+function sumStr(a, b) {
+  return (+a + +b).toString();
+}
+
+// console.log(sumStr("4", "5"));
+
+// alt options
+
+// function sumStr(a,b) {
+//   return String(Number(a)+Number(b));
+// }
+
+// sumStr = (a,b) => String(+a + +b);
+
+// function sumStr(a,b) {
+//   return String(Number(a)+Number(b));
+// }
