85225545086bfd30f8f64b97de82c9fc23e86537
mclark <maryariley4578@yahoo.com>
2023-11-15 19:05:07 -0500

    add coding challenges changes for 11-15-23

---
+++ CC 2023-11-15/app.js
@@ -0,0 +1,11 @@
+// 11.15.23
+
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+// stringToArray("I love arrays they are my favorite" );
+
+// const stringToArray = string => string.split(' ');
