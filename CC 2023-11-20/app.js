ed78d1316d274f9c01f3008ff290f8a1df43154b
mclark <maryariley4578@yahoo.com>
2023-11-20 19:02:24 -0500

    add coding challenges changes for 11-20-23

---
+++ CC 2023-11-20/app.js
@@ -0,0 +1,8 @@
+// 11.20.23 Sum Mixed Array
+
+// function sumMix(x){
+//    let result = x.map(str => {return parseInt(str, 10)});
+//    return result.reduce((acc, curr) => acc + curr, 0);
+// }
+
+// console.log(sumMix([2, 3, 4, "5", "8"]))
