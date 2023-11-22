06ca386e2de3d7f299e8dd870a6020626a7dfea0
mclark <maryariley4578@yahoo.com>
2023-11-21 19:00:37 -0500

    add coding challenges changes for 11-21-23

---
+++ CC 2023-11-21/app.js
@@ -0,0 +1,20 @@
+// 11.21.23  Sum Arrays
+
+// function sum(numbers) {
+//   return numbers.reduce((a, b) => a + b, 0);
+// }
+
+// If you can't sleep, just count sheep!!   11.28.23
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers
+
+// var countSheep = function (num){
+//   let n = '';
+//   for(let i = 1; i <= num; i++) {
+//     n += `${i} sheep...`;
+//   }
+//   return n;
+// }
+
+// console.log(countSheep(3));
