86fa638c57c7c1a36ca32d5e971a8d83aaebecdb
mclark <maryariley4578@yahoo.com>
2023-11-30 07:46:09 -0500

    add coding challenges changes for 11-30-23

--- CC 2023-11-30/app.js
+++ CC 2023-11-30/app.js
@@ -2,7 +2,7 @@
 
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
-// Examples
+// Examples for code
 
 // highAndLow("1 2 3 4 5");  // return "5 1"
 // highAndLow("1 2 -3 4 5"); // return "5 -3"
